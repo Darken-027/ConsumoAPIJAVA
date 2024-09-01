066a2e5a7bf870bef6c86d8def7d3b227c9420d0
Darken-027 <torut1638@gmail.com>
2024-08-31 22:11:38 -0500

    planeacion de flujo del programa

---
+++ src/consumoAPI/ConsumoAPI.java
@@ -0,0 +1,13 @@
+package consumoAPI;
+
+public class ConsumoAPI {
+    //Solicitar una peticion
+
+
+    //Comprobar si la peticion ha sido correcta
+
+
+    //Encaso de que peticion es correcta -- abrir un scanner que lea el flujo de datos
+
+    //Pintar la informacion odtenidda en consola
+}
