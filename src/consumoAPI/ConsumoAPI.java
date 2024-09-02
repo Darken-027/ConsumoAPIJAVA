7525e1737bb0dd4f86e63392f991c0f217350b67
Darken-027 <torut1638@gmail.com>
2024-09-01 22:37:31 -0500

    cambiaso

--- src/consumoAPI/ConsumoAPI.java
+++ src/consumoAPI/ConsumoAPI.java
@@ -2,9 +2,13 @@ package consumoAPI;
 
 public class ConsumoAPI {
     //Solicitar una peticion
-
+    
 
     //Comprobar si la peticion ha sido correcta
+        //Comprobar si la peticion ha sido correcta
+
+            //Comprobar si la peticion ha sido correcta
+
 
 
     //Encaso de que peticion es correcta -- abrir un scanner que lea el flujo de datos
